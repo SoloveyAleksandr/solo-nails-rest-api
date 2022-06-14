6ef48aff5a35d0013ec54790ae0e1651d7b9a4e1
hyper-design <hyper-design@keramin.int>
2022-06-14 14:16:05 +0300

    fix getNumberOfDays controller

--- controllers/DayController.js
+++ controllers/DayController.js
@@ -11,7 +11,6 @@ moment.updateLocale('en', {
 function DayController() {
 
   this.getNumberOfDays = (req, res) => {
-    console.log(req.params);
     const dateIsActual = Number(req.params.year.slice(1)) > 0;
 
     const year = dateIsActual ? req.params.year.slice(1) : moment().year();
@@ -19,38 +18,36 @@ function DayController() {
       req.params.month.length > 2 ? req.params.month.slice(1) : `0${req.params.month.slice(1)}`
       : moment().format('DD.MM.YYYY').slice(3, 5);
 
-    if (!dateIsActual) {
-      return res.send({
-        calendarDays: [],
-        year: Number(year),
-        month: Number(month),
-      })
-    } else {
-      const startOfWeek = moment(`01.${month}.${year}`, 'DD.MM.YYYY').startOf('month').startOf('week').subtract(1, 'day');
-      const calendarDays = [...Array(42)].map(() => {
-        const day = startOfWeek.add(1, 'day').clone();
-        const dayFormated = day.format('DD.MM.YYYY');
-        const isWeekend = day.day() === 6 || day.day() === 0 ? true : false;
-        const isPrevMonth = day.month() + 1 < month ? true : false;
-        const isNextMonth = day.month() + 1 > month ? true : false;
-        const isToday = moment().format('DD.MM.YYYY') === day.format('DD.MM.YYYY') ? true : false;
+    const startOfWeek = moment(`01.${month}.${year}`, 'DD.MM.YYYY').startOf('month').startOf('week').subtract(1, 'day');
+    const calendarDays = [...Array(42)].map(() => {
+      const day = startOfWeek.add(1, 'day').clone();
+      const dayFormated = day.format('DD.MM.YYYY');
+      const dayMonth = day.month() + 1;
+      const isWeekend = day.day() === 6 || day.day() === 0 ? true : false;
 
-        return {
-          fullDate: day,
-          day: dayFormated.slice(0, 2),
-          month: dayFormated.slice(3, 5),
-          year: dayFormated.slice(6),
-          isWeekend,
-          isPrevMonth,
-          isNextMonth,
-          isToday,
-        }
-      });
+      const isNextMonth = (dayMonth > Number(month) && day.year() >= year) ||
+        (day.year() > year) ? true : false;
 
-      return res.send({
-        calendarDays,
-      })
-    }
+      const isPrevMonth = (isNextMonth === false && dayMonth !== Number(month)) ? true : false;
+
+      const isToday = moment().format('DD.MM.YYYY') === dayFormated ? true : false;
+      return {
+        fullDate: day,
+        day: dayFormated.slice(0, 2),
+        month: dayFormated.slice(3, 5),
+        year: dayFormated.slice(6),
+        isWeekend,
+        isPrevMonth,
+        isNextMonth,
+        isToday,
+      }
+    });
+
+    return res.send({
+      calendarDays,
+      year: Number(year),
+      month: Number(month),
+    })
   };
 
   this.getDay = async (req, res) => {
